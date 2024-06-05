5a666537d71425d1a893623a6ee14c7d6af7e5cd
apaoluzzi <apaoluzzi@mac.com>
2024-06-05 10:01:27 +0200

    URGENTE

---
+++ PLASM-REVISE/HPC-LAR-HPC.jl
@@ -0,0 +1,21 @@
+#/////////////////////////////////////////////////////
+# trasforma la funzione HPC --> LAR in una nuova ma banale
+
+Lascia che riporti SOLO l'array non vuoto delle celle di massimo ordine computabili attraverso QHULL ...
+Se servono anche quelle di altri ordini, vedi sotto
+
+# LAR /////////////////////////////////////////////////////
+# l'attuale funzione HPC --> LAR
+
+Chiamala  __COMPLEX__  perché trasforma un dataset HPC nel complesso
+polyhedrale associato, coerente con il libro
+
+IMPORTANTE ... si adegua al libro ... e semplifica gli esempi ...
+
+#  Attualmente sono troppo limitato 
+
+#  ovviamente devo poter visualizzare se faccio  VIEW(HPC(COMPLEX(obj))):  basta visualizzare le celle d'ordine massimo ... 
+
+Chiama la nuova funzione HPC (per simmetria con LAR) se non vuoi fare un nuovo costruttore 
+
+#  Non mi far passare per forza attraverso MKPOL, perché rende brutti e troppo complicati molti esempi, che al momento non ho potuto mettere nel libro per questo motivo
